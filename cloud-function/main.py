d0835a010415e956cbdb17deee1315b9b9302849
Josh <joshrawlinson05@outlook.com>
2025-07-17 12:23:42 +0100

    Update main.py
    Receives a JSON file with timestamped temperature value

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -1,9 +1,22 @@
+# /main.py
 import functions_framework
+from pidController import PID_Controller
 
+pi = PID_Controller(Kp=-32000, Ki=-138560, Kd = 0, setpoint = 295)
 @functions_framework.http
-def echo(request):
+def controller(request):
     """
-    HTTP-Triggered function that echoes incoming data back to source
+    Recives a JSON file containing a time-stamped temperature data value
+    Returns a cooling value for recieved data
     """
-    request_data = request.get_data()
-    return request_data
+    request_json = request.get_json(silent=TRUE)
+    if request_json is None:
+        return "Invalid JSON: Request body is empty or not valid JSON", 400
+  
+    current_temp = request_json['temp']
+    dt = request_json['dt']
+
+    pi_value = pi.feedback(current_temp, dt)
+    cooling_power = {'coolingPower': max(0, pi_value)}
+
+    return cooling_power
