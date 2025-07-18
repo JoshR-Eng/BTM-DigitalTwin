a957236d2c8d5e2d791462512ed9c03611121bf2
JoshR <joshrawlinson05@outlook.com>
2025-07-18 11:20:38 +0100

    Fixed cloud code

--- cloud-function/main.py
+++ cloud-function/main.py
@@ -2,21 +2,30 @@
 import functions_framework
 from pidController import PID_Controller
 
-pi = PID_Controller(Kp=-32000, Ki=-138560, Kd = 0, setpoint = 295)
+pi = PID_Controller(Kp=-5.21, Ki=-1.12, Kd = 0, setpoint = 295)
 @functions_framework.http
 def controller(request):
     """
-    Recives a JSON file containing a time-stamped temperature data value
+    Recives a JSON file containing temperature
     Returns a cooling value for recieved data
     """
+    server_recieve_time = datetime.utcnow().isoformat() + "Z"
+
     request_json = request.get_json(silent=True)
     if request_json is None:
         return "Invalid JSON: Request body is empty or not valid JSON", 400
     
-    current_temp = request_json['temp']
+    current_temp = request_json['temperature']
     dt = request_json['dt']
+    client_send_time = request_json.get('client_send', None)
 
     pi_value = pi.feedback(current_temp, dt)
     cooling_power = {'coolingPower': max(0, pi_value)}
+
+    response = {
+        "Cooling_power": cooling_power,
+        "client_send_time": client_send_time,
+        "server_recieve_time": server_recieve_time
+    }
 
     return cooling_power
